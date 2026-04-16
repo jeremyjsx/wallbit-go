f3246469eedc789c7aa68c5f515959c058f1e746
JeremyDevCode <jeremydevcode@gmail.com>
2026-04-15 22:09:06 -0500

    feat(client): add authenticated request builder

--- client/client.go
+++ client/client.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"context"
 	"errors"
+	"io"
+	"net/http"
 	"strings"
 )
 
@@ -39,4 +42,24 @@ func NewClient(apiKey string, opts ...Option) (*Client, error) {
 
 func (c *Client) Config() *Config {
 	return c.cfg
+}
+
+func (c *Client) newRequest(ctx context.Context, method string, path string, body io.Reader) (*http.Request, error) {
+	endpoint, err := c.cfg.BaseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, endpoint.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("X-API-Key", c.apiKey)
+	req.Header.Set("User-Agent", c.cfg.UserAgent)
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, nil
 }
